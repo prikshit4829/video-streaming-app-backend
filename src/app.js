4a9b523d527300f90dbc1361a48a84d6cfdbc40d
Prikshit Sharma <prikshit4829@gmail.com>
2024-04-05 23:46:56 +0530

    updated utils

---
+++ src/app.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+const app = express();
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials:true
+}))
+
+// to let express know we accept json object
+app.use(express.json({
+    limit:"16kb"
+}))
+
+// to let express know we except from url (url can be of diffent type (encoding type) like %20 , + ) for this we use url encoded
+app.use(express.urlencoded({
+    extended:true,
+    limit:"16kb"
+}))
+
+// to accept and store file(img,favicon,pdf etc.) in server 'public folder
+app.use(express.static("public"))
+
+// to get access of browser cookies read/write
+app.use(cookieParser())
+
+export {app} 
+
+
+
+
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,22 @@
+const asyncHandler = (requestHandler)=>{
+    (req,res,next)=>{
+        Promise
+        .resolve(requestHandler(req,res,next))
+        .catch((error)=> next(error))
+    }
+}
+export {asyncHandler}
+
+/*
+// higher order function (take another function and makes wrapper around it (same as above)
+const asyncHandler = (fn) =>  async (req,res,next)=>{
+    try {
+        await fn(req,res,next);
+    } catch (error) {
+        res.status(error.code || 500).json({
+            success:false,
+            message:error.message
+        });
+    }
+}
+*/
